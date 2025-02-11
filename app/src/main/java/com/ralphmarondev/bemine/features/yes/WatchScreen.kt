97ffdeaba5a93498ea610c067f79500ba643c12b
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-02-11 12:31:17 +0800

    implemented WatchScreen

--- app/src/main/java/com/ralphmarondev/bemine/features/yes/WatchScreen.kt
+++ app/src/main/java/com/ralphmarondev/bemine/features/yes/WatchScreen.kt
@@ -1,14 +1,21 @@
 package com.ralphmarondev.bemine.features.yes
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBackIosNew
 import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -18,21 +25,57 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
 import com.ralphmarondev.bemine.R
-import com.ralphmarondev.bemine.core.components.LottieComponent
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WatchScreen(
-    navigateBack : () -> Unit,
+    navigateBack: () -> Unit,
     onNext: () -> Unit
 ) {
+    var selected by remember { mutableIntStateOf(-1) }
+    val movies = listOf(
+        MovieModel(
+            image = R.drawable.watch4_animated,
+            description = "Animated",
+            selected = selected == 0,
+            onClick = {
+                selected = 0
+            }
+        ),
+        MovieModel(
+            image = R.drawable.watch3_action,
+            description = "Action",
+            selected = selected == 1,
+            onClick = { selected = 1 }
+        ),
+        MovieModel(
+            image = R.drawable.watch2_scifi,
+            description = "Sci-Fi",
+            selected = selected == 2,
+            onClick = { selected = 2 }
+        ),
+        MovieModel(
+            image = R.drawable.watch1_horror,
+            description = "Horror",
+            selected = selected == 3,
+            onClick = { selected = 3 }
+        )
+    )
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -65,20 +108,34 @@ fun WatchScreen(
                 .padding(innerPadding),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            LottieComponent(
-                path = R.raw.so_happy,
+            Text(
+                text = "Pick a movie.",
+                color = MaterialTheme.colorScheme.secondary,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(300.dp)
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = "Pick a movie!",
-                color = MaterialTheme.colorScheme.secondary,
-                modifier = Modifier.padding(16.dp),
-                textAlign = TextAlign.Center,
+                    .padding(16.dp),
+                textAlign = TextAlign.Start,
                 fontSize = 20.sp
             )
+
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                modifier = Modifier
+                    .padding(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(movies.size) { index ->
+                    MovieCard(
+                        image = movies[index].image,
+                        description = movies[index].description,
+                        modifier = Modifier
+                            .weight(1f),
+                        selected = movies[index].selected,
+                        onClick = movies[index].onClick
+                    )
+                }
+            }
 
             Spacer(modifier = Modifier.weight(1f))
             Button(
@@ -92,6 +149,54 @@ fun WatchScreen(
                     textAlign = TextAlign.Center
                 )
             }
+        }
+    }
+}
+
+private data class MovieModel(
+    val image: Int,
+    val description: String,
+    val selected: Boolean,
+    val onClick: () -> Unit
+)
+
+@Composable
+private fun MovieCard(
+    modifier: Modifier = Modifier,
+    image: Int,
+    description: String,
+    selected: Boolean,
+    onClick: () -> Unit
+) {
+    ElevatedCard(
+        modifier = modifier
+            .border(
+                width = 1.dp,
+                color = if (selected) MaterialTheme.colorScheme.onSecondaryContainer else Color.Transparent,
+                shape = RoundedCornerShape(8.dp)
+            ),
+        onClick = onClick
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = rememberAsyncImagePainter(image),
+                contentDescription = description,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(140.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = description,
+                color = MaterialTheme.colorScheme.tertiary
+            )
         }
     }
 }
